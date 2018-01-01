855093bb0d182ce16c25e90d3989ea6088d83beb
microasif <asif0531@live.com>
2018-01-02 03:01:07 +0600

    vehicle insertation done

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,15 +1,35 @@
+using System.Threading.Tasks;
+using AutoMapper;
+using dotnetCore.Persistent;
 using Microsoft.AspNetCore.Mvc;
+using S.Controllers.Resources;
 using S.model;
 
 namespace S.Controllers
-{ 
+{
     [Route("api/vehicles")]
     public class VehiclesController : Controller
     {
-        [HttpPost]
-        public IActionResult GetAllvehicles([FromBody] Vehicle vehicle){ 
-            return Ok(vehicle);
+        private readonly IMapper mapper;
+        private readonly AppDbContext context;
+        public VehiclesController(AppDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+
         }
-        
+
+
+        [HttpPost]
+        public async Task<IActionResult> GetAllvehicles([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+            
+            var result = Mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+
     }
 }
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using dotnetCore.Controllers.Resources;
 using dotnetCore.model;
@@ -14,13 +15,24 @@ namespace dotnetCore.Mapping
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Features, KeyValuePairResource>();
+            CreateMap<Vehicle, VehicleResource>()
+                .ForMember(vr => vr.Contact, opt => opt.MapFrom(v => new ContactResource
+                 {  Name = v.ContactName, Email = v.ContactEmail, Phone = v.ContactPhone  }))
+                 .ForMember(vt => vt.Features, opt => opt.MapFrom(v => v.Features.Select(x=>x.FeatureId)));
 
 
             //api to domain resources 
-            CreateMap<VehicleResource, Vehicle>()
-            .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vt => vt.Contact.Phone))
-            .ForMember(v => v.ContactName, opt => opt.MapFrom(vt => vt.Contact.Name))
-            .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vt => vt.Contact.Email));
+             CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.Features, opt => opt.MapFrom(vt => vt.Features
+                .Select(id => new VehicleFeature {  
+                    FeatureId = id
+                })));
+
+
+            
         }
     }
 }
